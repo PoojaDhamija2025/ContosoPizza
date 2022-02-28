ec96fd72adddf9e3a4b01323800fe9b96a80df82
Pooja_dhamija2025 <Pooja.Dhamija@Philips.com>
2022-02-28 15:56:32 +0530

    Added Model Validations

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 namespace ContosoPizza.Models;
 
 public class Pizza
@@ -5,10 +6,13 @@ public class Pizza
     private List<string> _toppings = new List<string>();
     public enum LocalityChosen { East, West, North, South };
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
+    [Range(typeof(bool), "true", "true", ErrorMessage="Value for this field must be provided.")]
     public bool IsGlutenFree { get; set; }
+    [Required, MinLength(1)]
     public List<string> Toppings{  get { return _toppings; }
     set { _toppings = value; }}
+    [EnumDataType(typeof(LocalityChosen))]
     public LocalityChosen DistributionLocality{ get; set; }
   
 }
